32d10056060115fb1ed88b17e27784c478a88603
kamranlatifch <kamranlatif98@gmail.com>
2023-05-05 11:35:01 +0500

    Group Booking page completed

--- src/Components/HowToBook/GroupBookings/GroupBookings.jsx
+++ src/Components/HowToBook/GroupBookings/GroupBookings.jsx
@@ -3,7 +3,84 @@ import React from "react";
 function GroupBookings() {
   return (
     <div>
-      <h1>This is Group Booking Page</h1>
+      <div className="col-12 right-div-data">
+        <div className="my-2 d-flex">
+          <p>
+            Making a group booking is easy with{" "}
+            <span style={{ fontSize: 20, fontWeight: "bold" }}> FLYONE</span>{" "}
+            because we offer special group fares with a variety of options and
+            benefits.
+            <br />
+            <br />
+            If you are travelling in a group of more than 10 people to{" "}
+            <span style={{ fontSize: 20, fontWeight: "bold" }}>FLYONE</span>
+            destinations, you can benefit from a special group fare.
+            <br />
+            <br />
+            <span style={{ fontSize: 20, fontWeight: "bold" }}>
+              {" "}
+              How can you get this group fare?
+            </span>
+            <br />
+            It has never been easier. All you have to do is first send us a
+            request through Contacts , select{" "}
+            <span style={{ fontSize: 20, fontWeight: "bold" }}>
+              {" "}
+              Request for Group Bookings
+            </span>{" "}
+            and we will do our best to handle your request.
+            <br />
+            <br />
+            When sending the request, please enter the following travel details:
+            <br />
+            1. Destination
+            <br />
+            2. Date of flight
+            <br />
+            3. Number of passengers (adult / child / infant)
+            <br />
+            4. Tour package
+            <br />
+            <br />
+            *Please be informed that when traveling with FLYONE, you can choose
+            one of the fare groups and their benefits You will receive the
+            answer to your request with all the details within 24 hours.
+            <br />
+            <br />
+            <span style={{ fontSize: 20, fontWeight: "bold" }}>
+              {" "}
+              How can the payment be made for group booking?
+            </span>
+            <br />
+            You will receive details about the company fares and payment
+            deadline.
+            <br />
+            When confirming the group booking, a non-refundable fee of 50% of
+            the total amount is payable.
+            <br />
+            <br />
+            <span style={{ fontSize: 20, fontWeight: "bold" }}>
+              {" "}
+              When are passenger details and their names entered?
+            </span>
+            <br />
+            Names of passengers can be entered only if the full amount is paid.
+            Passenger details may be entered / modified up to 3 days before the
+            scheduled departure.
+            <br />
+            <br />
+            <span style={{ fontSize: 20, fontWeight: "bold" }}>
+              {" "}
+              Payment Method
+            </span>
+            <br />
+            Payment for group booking can be made by bank transfer. You will
+            receive an email about the details of this payment method.
+            <br />
+            <br />
+          </p>
+        </div>
+      </div>
     </div>
   );
 }

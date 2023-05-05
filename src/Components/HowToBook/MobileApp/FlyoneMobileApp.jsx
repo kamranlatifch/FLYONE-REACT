0adc5980f316ba167c8122f9e75f708112edb4ca
kamranlatifch <kamranlatif98@gmail.com>
2023-05-05 11:49:20 +0500

    how to book completed

--- src/Components/HowToBook/MobileApp/FlyoneMobileApp.jsx
+++ src/Components/HowToBook/MobileApp/FlyoneMobileApp.jsx
@@ -1,9 +1,80 @@
 import React from "react";
-
+import app from "./app.png";
 function FlyoneMobileApp() {
   return (
     <div>
-      <h1>This is Flyone Mobile App Page</h1>
+      <div className="col-12 right-div-data">
+        <div className="row">
+          <img src={app} alt="voucher" />
+        </div>
+        <div className="row my-2">
+          <div className="col-10">
+            <b>
+              {" "}
+              Plan your trips from anywhere - simple, convenient and affordable.
+            </b>
+            <br />
+            <br />
+            Download the FLYONE app - an interactive, intuitive and
+            user-friendly platform for booking flights, checking in, receiving
+            discounts, using vouchers and testing other smart options.
+            <br />
+            <br />
+            <br />
+            <b>
+              SEARCH FOR YOUR FAVORITE DESTINATION AND BOOK TRIP IN JUST 3 STEPS
+            </b>
+            <br />
+            <br />
+            FLYONE operates flights to 14 destinations: Frankfurt, Tel Aviv, St.
+            Petersburg, London, Moscow, Parma, Verona, Dublin, Lisbon, Paris,
+            Antalya, Heraklion, Sharm El-Sheikh, Bodrum. Choose the city and
+            travel period, add services for a pleasant flight and pay by card.
+            <br />
+            <br />
+            <br />
+            <b>SAVE PRECIOUS TIME</b>
+            <br />
+            <br />
+            Create your personal profile and use the automatic completion of
+            personal and banking data.
+            <br />
+            <br />
+            <br />
+            <b>SAVE ALL THE INFORMATION ON ONE DEVICE</b>
+            <br />
+            <br />
+            Check in to the app, get your boarding pass on your phone and scan
+            it at the airport.
+            <br />
+            <br />
+            <br />
+            <b>VIEW TRAVEL HISTORY</b>
+            <br />
+            <br />
+            The application reminds you of past searches and trips, to plan
+            other flights that you might forgot.
+            <br />
+            <br />
+            <br />
+            <b> BE THE FIRST TO FIND OUT OFFERS AND DISCOUNTS</b>
+            <br />
+            <br />
+            Find out about hot promotions and offers. Don't miss the chance to
+            fly cheap to your favorite destinations.
+            <br />
+            <br />
+            <br />
+            <b>
+              {" "}
+              Plan your trips in the FLYONE app - cheap flights just a few
+              clicks away!
+            </b>
+          </div>
+        </div>
+        <br />
+        <br />
+      </div>
     </div>
   );
 }

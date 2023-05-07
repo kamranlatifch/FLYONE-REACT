e3b3170abac58122bbaaafe36118b9d4216a9f75
kamranlatifch <kamranlatif98@gmail.com>
2023-05-07 22:23:58 +0500

    menu on board pdf files linked

--- src/Components/BeforeFlight/MenuOnBoard/MenuOnBoard.jsx
+++ src/Components/BeforeFlight/MenuOnBoard/MenuOnBoard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import Menu3 from "./Menu 3F.pdf";
+import Menu5 from "./MENU 5F.pdf";
 function MenuOnBoard() {
   return (
     <div>
@@ -10,8 +11,29 @@ function MenuOnBoard() {
         or soft drinks and other delicacies. You can also pre-order one of the{" "}
         <Link style={{ color: "blue" }}> 3 sets of dishes online.</Link>
         <br /> <br />
-        <h4> MENU of FLYONE (5F) </h4> <br /> <br />
-        <h4>MENU of FLYONE ARMENIA (3F)</h4> <br /> <br />
+        <h4>
+          <a
+            href={Menu5}
+            download="Example-PDF-document"
+            target="_blank"
+            rel="noreferrer"
+          >
+            MENU of FLYONE (5F)
+          </a>
+        </h4>
+        <br /> <br />
+        <h4>
+          {" "}
+          <a
+            href={Menu3}
+            download="Example-PDF-document"
+            target="_blank"
+            rel="noreferrer"
+          >
+            MENU of FLYONE ARMENIA (3F){" "}
+          </a>
+        </h4>{" "}
+        <br /> <br />
         On our flights you will discover that everything is delicious!
       </p>
     </div>

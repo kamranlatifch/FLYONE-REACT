209bd86844f05efd92f1f3947c6d614f2c49d29a
kamranlatifch <kamranlatif98@gmail.com>
2023-05-07 22:11:12 +0500

    Menu on board page added

--- src/Components/BeforeFlight/MenuOnBoard/MenuOnBoard.jsx
+++ src/Components/BeforeFlight/MenuOnBoard/MenuOnBoard.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function MenuOnBoard() {
   return (
     <div>
-      <h1>I am Menu on board page</h1>
+      <p>
+        <h4>You can find the FLYONE menu on every flight.</h4> <br />
+        As Delight your taste buds with tea, coffee, sweets or snacks, alcoholic
+        or soft drinks and other delicacies. You can also pre-order one of the{" "}
+        <Link style={{ color: "blue" }}> 3 sets of dishes online.</Link>
+        <br /> <br />
+        <h4> MENU of FLYONE (5F) </h4> <br /> <br />
+        <h4>MENU of FLYONE ARMENIA (3F)</h4> <br /> <br />
+        On our flights you will discover that everything is delicious!
+      </p>
     </div>
   );
 }

81aa5f93406458987488dd1b3e6130dd764320b6
kamranlatifch <kamranlatif98@gmail.com>
2023-05-08 10:56:53 +0500

    Security Articles Prohibited added

--- src/Components/BeforeFlight/Security-ArticlesProhibited/ArticlesProhibited.jsx
+++ src/Components/BeforeFlight/Security-ArticlesProhibited/ArticlesProhibited.jsx
@@ -3,7 +3,124 @@ import React from "react";
 function ArticlesProhibited() {
   return (
     <div>
-      <h1>I am security articles prohibited page</h1>
+      <h4>SECURITY: ARTICLES PROHIBITED OR LIMITED FOR TRANSPORTATION</h4>{" "}
+      <br />
+      Dangerous goods are articles or substances which are capable of posing a
+      risk to health, safety, property or the environment.
+      <br />
+      <br />
+      <b>Prohibited items in hand baggage</b>
+      <br />
+      <br />
+      <b>Workmen’s tools</b>
+      <br />
+      crowbars, drills and drill bits, including cordless portable power drills,
+      <br />
+      tools with a blade or a shaft of more than 6 cm capable of use as a
+      weapon, such as screwdrivers and chisels, saws, including cordless
+      portable power saws,
+      <br />
+      blowtorches,
+      <br />
+      bolt guns and nail guns;
+      <br />
+      <br />
+      <b>Objects with a sharp point or sharp edge</b>
+      <br />
+      items designed for chopping, such as axes, hatchets and cleavers, ice axes
+      and ice picks, razor blades,
+      <br />
+      box cutters,
+      <br />
+      knives with blades of more than 6 cm,
+      <br />
+      scissors with blades of more than 6 cm measured from the fulcrum,
+      <br />
+      material arts equipment with a sharp point or sharp edge, swards and
+      sabers
+      <br />
+      <br />
+      <b>Stunning/ paralyzing effect devices</b>
+      <br />
+      devices for shocking, such as stun guns, tasers and stun batons,
+      <br />
+      animal stunners and animal killers,
+      <br />
+      disabling and incapacitating chemicals, gases and sprays, such as mace,
+      pepper sprays, capsicum sprays, tear gas, <br />
+      acid sprays and animal repellent sprays;
+      <br />
+      <br />
+      <b>Blunt instruments</b>
+      <br />
+      baseball and softball bats,
+      <br />
+      clubs and batons, such as billy clubs, blackjacks and night sticks,
+      <br />
+      martial arts equipment.
+      <br />
+      <br />
+      <b>Prohibited items in hand baggage and checked baggage</b>
+      <br />
+      <br />
+      <b>Explosives and incendiary substances and devices:</b>
+      <br />
+      ammunition,
+      <br />
+      blasting caps,
+      <br />
+      detonators and fuses,
+      <br />
+      replica or imitation explosive devices,
+      <br />
+      mines, grenades and other explosive military stores,
+      <br />
+      fireworks and other pyrotechnics,
+      <br />
+      smoke-generating canisters and smoke-generating cartridges,
+      <br />
+      dynamite gunpowder and plastic explosives,
+      <br />
+      <br />
+      <b>Guns, firearms or other devices:</b>
+      <br />
+      firearms of all types, such as pistols, revolvers, rifles, shotguns,
+      <br />
+      toy guns, replicas and imitation firearms capable of being mistaken for
+      real weapons,
+      <br />
+      component parts of firearms, excluding telescopic sights,
+      <br />
+      compressed air or CO2 guns, such as pistols, pellet guns, rifles and ball
+      bearing guns,
+      <br />
+      signal flare pistols and starter pistols,
+      <br />
+      bows, cross bows and arrows,
+      <br />
+      harpoon guns and spear guns,
+      <br />
+      slingshots and catapults;
+      <br />
+      <br />
+      <b>Fire extinguisher</b>
+      <br />
+      <br />
+      <b>Fireworks and other pyrotechnics, flares;</b>
+      <br />
+      <br />
+      <b>Compressed gases (e.g. butane, propane, lighter fuel, oxygen)</b>
+      <br />
+      <br />
+      <b>Electronic cigarettes</b>
+      <br />
+      <br />
+      <b>Batteries: lithium metal and lithium ion batteries</b>
+      <br />
+      <br />
+      <b>Flammable liquids</b>
+      <br />
+      <br />
     </div>
   );
 }

1e0335fe2af6fcdebbd4f54b4db978167b203857
kamranlatifch <kamranlatif98@gmail.com>
2023-05-08 10:45:08 +0500

    Liquids page added

--- src/Components/BeforeFlight/Liquids/Liquids.jsx
+++ src/Components/BeforeFlight/Liquids/Liquids.jsx
@@ -1,9 +1,117 @@
 import React from "react";
-
+import liquids from "./Liquids.png";
 function Liquids() {
   return (
     <div>
-      <h1>I am liquids Page</h1>
+      <img src={liquids} alt="Liquids" />
+      <p>
+        <br />
+        <br />
+        <h4>EU RULES OF SECURITY REGARDING TRANSPORTING LIQUIDS</h4>
+        <br />
+        The European Union has adopted a set of rules of security, which
+        strictly limit the quantity of liquids that you can transport in your
+        luggage.
+        <br />
+        <br />
+        Some of the liquids are:
+        <br />
+        <li>
+          Beverages, including water and other refreshing drinks, soups, syrups
+        </li>
+        <br />
+        <li>Creams, lotions and oils</li>
+        <br />
+        <li>Perfume</li>
+        <br />
+        <li>Deodorant spray</li>
+        <br />
+        <li>Gels, including hair gels and shower gels</li>
+        <br />
+        <li>
+          The content of the recipients under pressure, including shaving foam,
+          other foams and deodorants
+        </li>
+        <br />
+        <li>Mixtures of liquid and solid substances</li>
+        <br />
+        <li>Cosmetic products for make-up</li>
+        <br />
+        <li>Other items with similar consistency</li>
+        <br />
+        <li>When you pack</li>
+        <br />
+        Take into account that each liquid must be in its own recipient, whose
+        capacity must not exceed 100 ml. These liquids will not exceed the
+        maximum limit of 1000 ml per person/passenger, all taken together. The
+        recipients must be packed in plastic, transparent and resealable bags.
+        The items must be loosely stored and the bag must be sealed. Only one
+        bag with liquids is allowed per passenger.
+        <br />
+        <br />
+        The bag must be presented separately at the airport’s security control.
+        These rules apply to all of the passengers leaving from EU airports, no
+        matter the destination.
+        <br />
+        <br />
+        Passengers departing from a non-EU country landing in one EU country
+        will comply with these rules. Remember, you will not be allowed to
+        transport liquids in a quantity bigger than 1000 ml in the hand luggage,
+        given the above conditions.
+        <br />
+        <br />
+        It is allowed:
+        <br />
+        To introduce liquids in the registered luggage, the new rules are
+        applicable only to the hand luggage
+        <br />
+        To take medicine and the necessary products to a special diet, including
+        products for the babies, destined to be used during the trip. You may be
+        requested to prove that the products will be used on board.
+        <br />
+        To buy liquids, like drinks or perfumes at the shops in the EU airports,
+        placed far away from where you are requested to present your boarding
+        ticket or from the board of the EU operated airlines. If these liquids
+        are sold in special sealed bags, do not open them before passing the
+        security control, otherwise, the content of the bag may be confiscated
+        at the control points (if you are transferring from one airport to
+        another in EU, do not open the bag before passing the control at the
+        transfer airport, or at the last transfer airport, if you get
+        transferred several times).
+        <br />
+        These liquids are additional to the quantities stipulated for the bags
+        with closing/opening elements, mentioned above.
+        <br />
+        <br />
+        <br />
+        <li>
+          Help the security personnel identify the liquids! In order to do that,
+          you must:
+        </li>
+        <br />
+        <li>
+          Present all of the liquid substances that you have to the security
+          personnel, in order to get examined
+        </li>
+        <br />
+        <li>
+          Take off all coats, trenches and jackets. These are controlled
+          separately
+        </li>
+        <br />
+        <li>
+          Take the portable calculators along with the rest of the electronic
+          devices out from the hand luggage. They will be controlled separately
+        </li>
+        <br />
+        <li>
+          In the airplane’s cabin you can transport living animals (dogs and
+          cats) and in order to get acquainted with the rules of transporting
+          animals, access the category Transporting pets.
+        </li>
+        <br />
+        <br />
+      </p>
     </div>
   );
 }

0b979cb636f1142c0804a4e697fd0206b4b32192
kamranlatifch <kamranlatif98@gmail.com>
2023-05-08 10:51:38 +0500

    Zero Tolernce Page added

--- src/Components/BeforeFlight/ZeroTolerancePolicy/ZeroTolerancePolicy.jsx
+++ src/Components/BeforeFlight/ZeroTolerancePolicy/ZeroTolerancePolicy.jsx
@@ -3,7 +3,69 @@ import React from "react";
 function ZeroTolerancePolicy() {
   return (
     <div>
-      <h1>Zero tolerance policy page</h1>
+      <h4>GENERAL POLICY</h4>
+      <br />
+      <br />
+      <p>
+        Our main objective is to ensure an excellent level of safety and
+        security for all FLYONE’s customers and staff. Unruly passengers can
+        create serious safety hazards by interfering with the operation of an
+        aircraft. These are a nuisance to other passengers and a danger to
+        themselves in an emergency situation.
+        <br />
+        <br />
+        Unruly passengers will be reported to the responsible authorities and
+        may face legal consequences (fines, retention).
+        <br />
+        <br />
+        Passengers should be aware that FLYONE is permitted to deny boarding to
+        anyone who behaves in an unruly or disruptive manner. The unruly
+        passenger is not entitled to a refund.
+        <br />
+        <br />
+        FLYONE believes that prevention is the main strategy in dealing with
+        unruly passengers and acts accordingly.
+        <br />
+        <br />
+        WARNING: FLYONE declares Zero Tolerance Policy to be applied!
+        <br />
+        <br />
+        FLYONE Zero Tolerance Policy:
+        <br />
+        - empowers crews and ground staff to take reasonable steps to prevent
+        disruptive and drunken behavior and, to deal with it as effectively as
+        practicable including refusal of carriage of passengers who have the
+        potential to create the disturbances;
+        <br />
+        - supports crews and ground staff taking such action;
+        <br />
+        - encourages the police to prosecute disruptive passengers in
+        appropriate cases, especially when there have been assaults on FLYONE
+        staff;
+        <br />
+        - assists and supports crew members and ground staff who are required,
+        after an incident, to give witness statements to the police or to appear
+        in court proceedings when unruly passengers are prosecuted;
+        <br />
+        - provides appropriate training to crew and ground staff in dealing with
+        conflicts;
+        <br />
+        - denies future carriage to abusive passengers who remain a threat to
+        employees of the company;
+        <br />
+        - encourages Ground Staff to detect and report disruptive behavior at
+        check-in, in the lounges and at the boarding gate in order to prevent
+        such passengers from boarding;
+        <br />
+        - Keeps Ground Staff, Cabin Crew and Flight Crew aware of potentially
+        disruptive passengers.
+        <br />
+        <br />
+        FLYONE policy applies equally to all passengers regardless of their
+        class of travel or status.
+        <br />
+        <br />
+      </p>
     </div>
   );
 }

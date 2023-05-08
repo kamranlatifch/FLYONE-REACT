52dd1640a1872994701ae4464d873499b3c2fdce
kamranlatifch <kamranlatif98@gmail.com>
2023-05-08 10:48:40 +0500

    Smoking page added

--- src/Components/BeforeFlight/Smoking/Smoking.jsx
+++ src/Components/BeforeFlight/Smoking/Smoking.jsx
@@ -3,7 +3,25 @@ import React from "react";
 function Smoking() {
   return (
     <div>
-      <h1>I am smoking page</h1>
+      <h4>SMOKING ON THE AIRPLANE’S BOARD</h4>
+      <br />
+      <p>
+        As we have previously mentioned, the main objective of FLYONE is the
+        safety and comfort of its passengers. Therefore, in order to provide an
+        ecological pure space in its airplanes’ cabins, all of the company’s
+        flights are non-smoking.
+        <br />
+        <br />
+        Smoking is prohibited on the FLYONE’s airplanes boards and the smoke
+        detectors that are installed in the cabin and toilet help keep this
+        unchanged. We ask the passengers to respect this rule of behavior, whose
+        infringement may attract additional sanctions.
+        <br />
+        <br />
+        Thank you for your understanding!
+      </p>
+      <br />
+      <br />
     </div>
   );
 }

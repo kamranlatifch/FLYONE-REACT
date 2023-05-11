2616b6f8a0fe69a634ebd2f3198a245d428f1f0a
kamranlatifch <kamranlatif98@gmail.com>
2023-05-11 13:10:03 +0500

    Guide page

--- src/Components/BeforeFlight/GuideForSafeTravel/GuideForSafeTravel.jsx
+++ src/Components/BeforeFlight/GuideForSafeTravel/GuideForSafeTravel.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-
+import "./guide.css";
+import safetravel from "./Safe_Travel.jpg";
 function GuideForSafeTravel() {
   return (
-    <div>
-      <h1>I am guide for SAFE Travel</h1>
-    </div>
+    <>
+      <img src={safetravel} alt="Safe Travel" />
+      <br />
+      <br />
+      <h4>Play video</h4>
+      <br />
+      <h4>Preventive Measures for Safe and Pleasant Flights</h4>
+      <div className="col-12">
+        <div className="guideHeader">
+          <h5>Trip Planning</h5>
+        </div>
+      </div>
+    </>
   );
 }
 

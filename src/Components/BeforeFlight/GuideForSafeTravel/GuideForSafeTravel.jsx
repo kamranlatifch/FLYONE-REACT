23a515e7671792aefeeb8372694c292b08b52c9d
kamranlatifch <kamranlatif98@gmail.com>
2023-05-12 11:13:28 +0500

    commit

--- src/Components/BeforeFlight/GuideForSafeTravel/GuideForSafeTravel.jsx
+++ src/Components/BeforeFlight/GuideForSafeTravel/GuideForSafeTravel.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./guide.css";
 import safetravel from "./Safe_Travel.jpg";
+import tp1 from "./tp1.svg";
+import tp2 from "./tp2.svg";
+import tp3 from "./tp3.svg";
+import tp4 from "./tp4.svg";
 function GuideForSafeTravel() {
   return (
     <>
@@ -13,6 +17,21 @@ function GuideForSafeTravel() {
       <div className="col-12">
         <div className="guideHeader">
           <h5>Trip Planning</h5>
+        </div>
+      </div>
+      <div className="d-flex my-3">
+        <div className="d-flex">
+          <img src={tp1} alt="" style={{ width: 70 }} />
+          <p style={{ alignSelf: "center", marginLeft: 10 }}>
+            Online planning – book tickets on flyone.eu. Select online card
+            payment.
+          </p>
+        </div>
+        <div className="d-flex">
+          <img src={tp2} alt="" style={{ width: 70 }} />
+          <p style={{ marginLeft: "10px" }}>
+            Check-in online and get the boarding pass on your mobile.
+          </p>
         </div>
       </div>
     </>
